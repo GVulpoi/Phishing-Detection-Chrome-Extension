46ab07936d73902601ce31e7cd0a34b2cde27532
Vulpoi Gabriel <gvulpoi@yahoo.com>
2024-11-18 17:18:06 +0200

    Finished fetching from external and inline scripts!

--- Scripts/backround.js
+++
@@ -1,1 +0,0 @@
-console.log("Backround js is working!");
--- Scripts/retrieveScripts.js
+++ Scripts/retrieveScripts.js
@@ -1,21 +1,41 @@
+//Logistics for fetching scripts and url's of scripts files
 const scriptName = Array.from(document.querySelectorAll('script'));
 
-scriptName.forEach((script, index) => 
+const inlineScripts = scriptName
+								.filter(script => !script.src)
+								.map(script => script.textContent);
+
+const externalScripts = scriptName
+								.filter(script => script.src)
+								.map(script => script.src);
+
+console.log("Inline Scripts : ", inlineScripts);
+console.log("External Scripts : ", externalScripts);
+
+async function copyCodeFromFile(url)
 {
-	if (script.src)
-	{
-		fetch(script.src)
-			.then(Response => Response.text())
-			.then(code => 
+	fetch(url)
+			try
 			{
-				console.log('Script', index, ':', code);
+				const response = await fetch(url);
+
+				if (!response.ok)
+				{
+					throw new Error(`The response from url wasn't recieved ${response.statusText}`);
+				}
+
+				const data = await response.text();
+				console.log(`Script from url : ${url}`, data);
+				return data;
+
 			}
-				)
-			.catch(myErr => console.error('Failed to fetch script ${index}:', myErr));
-	}
-	else
-	{
-		console.log('Script', index, ':', script.innerHTML);
-	}
+			catch(err)
+			{
+				console.error(`Fetch operation has failed for url ${url}`, err);
+			}
 }
-					)
+
+externalScripts.forEach(	obj =>
+							{
+								copyCodeFromFile(obj);
+							});
