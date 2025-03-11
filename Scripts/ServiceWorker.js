bb6613eb77fe6fb41c31dd4792407cf4f5248f5b
Vulpoi Gabriel <gvulpoi@yahoo.com>
2025-03-11 16:37:11 +0200

    Fixed bug on sendResponse() and put updated rules for js disable!

--- Scripts/ServiceWorker.js
+++ Scripts/ServiceWorker.js
@@ -5,11 +5,12 @@ chrome.runtime.onInstalled.addListener(() => {
     console.log("Extension Installed!");
 });
 
-chrome.runtime.onMessage.addListener((message, sender, response) =>
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) =>
 {
     if (!message.action)
     {
         console.log("Recieved message without action! Message ignored!");
+        sendResponse({ status: "error"});
         return;
     }
 
@@ -20,10 +21,24 @@ chrome.runtime.onMessage.addListener((message, sender, response) =>
             sendResponse({ status: "success"});
             console.log(message.allScripts);
 
+            //Enabling the scripts
+            chrome.declarativeNetRequest.updateEnabledRulesets({
+                disableRulesetIds: ["block-scripts"]
+            }, () => {
+                if (chrome.runtime.lastError) {
+                    console.error("Failed to disable ruleset:", chrome.runtime.lastError);
+                    sendResponse({ status: "error", message: chrome.runtime.lastError.message });
+                } else {
+                    console.log("Ruleset disabled successfully!");
+                    sendResponse({ status: "success", message: "Ruleset disabled" });
+                }
+            });
+
             return true;
 
         default:
             console.log("Recieved message with unknown action!");
+            sendResponse({ status: "error"});
             return;
     }
 }
